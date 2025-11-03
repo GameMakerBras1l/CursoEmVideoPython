f3b83d7465ad435017a96aa0d34d5b168a7d2d51
GameMakerBras1l <marcelciro2015@gmail.com>
2025-11-03 18:04:31 -0300

    ATUALIZADO

--- Ex055.py
+++ Ex055.py
@@ -1,7 +1,24 @@
-pesos = [0, 0, 0, 0, 0]
+"""pesos = [0, 0, 0, 0, 0]
 
 for c in range(0, 5):
-    pesos[c] = int(input("Digite um peso: "))
+    pesos[c] = float(input("Digite um peso: "))
 
 pesos.sort()
-print(f"Os pesos foram {pesos}")
+print(f"Os pesos foram {pesos}")"""
+
+maior = 0
+menor = 0
+
+for p in range(1, 6):
+    peso = float(input("Peso da {}* pessoa: ".format(p)))
+    if p == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+
+print(f"O maior peso lido foi {maior}")
+print(f"O menor peso lido foi {menor}")
--- Ex056.py
+++ Ex056.py
@@ -1,7 +1,25 @@
-nome = ["", "", "", ""]
-idade = [0, 0, 0, 0]
-f = 0
+somaidade = 0
+mediaidade = 0
+maioridade = 0
+menoridade = 0
+nomevelho = ""
+for p in range (1, 5):
+    print(f"-----{p}* PESSOA -----")
+    nome = str(input("Nome: ")).strip()
+    idade = int(input("Idade: "))
+    sexo = str(input("Sexo [M/F]: ")).strip()
+    somaidade += idade
+    if p == 1 and sexo in "Mm":
+        maioridade = idade
+        nomevelho = nome
+    if sexo in "Mm" and idade > maioridade:
+        maioridade = idade
+        nomevelho = nome
 
-for c in range(4):
-    nome = print("Digite o nome de uma pessoa: ")
-    
+    if sexo in "Ff" and idade < 20:
+        menoridade += 1
+
+mediaidade = somaidade/4
+print(f"A Media de Idade do Grupo é de {mediaidade} anos.")
+print(f"O homem mais velho tem {maioridade} e seu nome é {nomevelho}.")
+print(f"O número de mulheres com menos de 20 anos são {menoridade}")
