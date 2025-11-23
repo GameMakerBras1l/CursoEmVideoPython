e22876c9a8ede9bb525bfeaddfc530f0e220b60d
GameMakerBras1l <marcelciro2015@gmail.com>
2025-11-23 12:13:35 -0300

    ATUALIZADO

---
+++ Ex058.py
@@ -0,0 +1,28 @@
+import random
+from time import sleep
+
+x = 0
+pc = random.randint(0,10)
+use = -1
+
+print("Vamos jogar um jogo tente adivinhar um número de 0 até 10")
+print("")
+
+while pc != use:
+    use = int(input("Digite un número de 0 até 10: "))
+    print("")
+
+    sleep(1)
+
+    if pc != use:
+        print("Você errou tente de novo")
+
+    x += 1
+
+print("")
+
+if x < 6:
+    print(f"Parabens vc acertou o numero em {x} tentativas")
+
+else:
+    print(f"Você precisou de {x} tentativas")
