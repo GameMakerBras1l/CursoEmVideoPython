1b8eae801222cc4e345cd173fde930ee22ffd477
GameMakerBras1l <marcelciro2015@gmail.com>
2026-01-28 22:45:33 -0300

    ATUALIZADO

--- Ex051-060/Ex058.py
+++ Ex051-060/Ex058.py
@@ -3,12 +3,16 @@ from time import sleep
 
 x = 0
 pc = random.randint(0,10)
-use = -1
+"""use = -1"""
+palp = 0
 
 print("Vamos jogar um jogo tente adivinhar um número de 0 até 10")
 print("")
 
-while pc != use:
+acertou = False
+
+"""while pc != use:
+
     use = int(input("Digite un número de 0 até 10: "))
     print("")
 
@@ -25,4 +29,17 @@ if x < 6:
     print(f"Parabens vc acertou o número em {x} tentativas")
 
 else:
-    print(f"Você precisou de {x} tentativas")
+    print(f"Você precisou de {x} tentativas")"""
+
+while not acertou:
+    use = int(input("Qual seu paltpite: "))
+    palp += 1
+    if use == pc:
+        acertou = True
+    else:
+        if use < pc:
+            print("Mais...")
+        elif use > pc:
+            print("Menos...")
+
+print("Acertou com {} tentarivas".format(palp))
