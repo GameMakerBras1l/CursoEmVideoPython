740eab442a04d5fcbaa5721167fc4ee82e99812a
GameMakerBras1l <marcelciro2015@gmail.com>
2026-02-13 10:03:55 -0300

    ATUALIZADO

--- Ex064.py
+++ Ex064.py
@@ -1,13 +1,14 @@
-n1 = n2 = 0
-count = -1
+n1 = n2 = count = 0
+
+n1 = int(input("Digite um número [999 para parar]: "))
 
 while n1 != 999:
-    print("Digite 999 quando quiser parar.")
-    n1 = int(input("Digite um número: "))
+    n1 = int(input("Digite um número [999 para parar]: "))
     n2 = n1 + n2
 
     count += 1
 
 n3 = n2 - 999
 
-print("Você digitou {} veses, e as soma foi de {}".format(count, n3))
+print("Você digitou {} veses, e as soma " \
+"foi de {}".format(count, n3))
